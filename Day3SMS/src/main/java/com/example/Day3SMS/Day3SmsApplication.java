c2ddbe5fafae6025491424369d9d2364bc71218d
shubhkesh05 <kesharwanishubh17@gmail.com.com>
2026-01-20 11:53:17 +0530

    first commit

---
+++ Day3SMS/src/main/java/com/example/Day3SMS/Day3SmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day3SMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3SmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3SmsApplication.class, args);
+	}
+
+}
---
+++ Day3SMS/src/test/java/com/example/Day3SMS/Day3SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day3SMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
