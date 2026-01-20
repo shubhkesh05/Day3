d90b3ffe35149a438901eccef4821f5d7ae3241a
shubhkesh05 <kesharwanishubh17@gmail.com.com>
2026-01-20 12:49:23 +0530

    update

---
+++ Day3SMS/src/main/java/com/example/Day3SMS/model/StudentModel.java
@@ -0,0 +1,20 @@
+package com.example.Day3SMS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "students")
+public class StudentModel {
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+
+}
